a0e0d3a4ce5939ee68771aee7cb72cf8c53778d8
kubakopala <kubakopala@gmail.com>
2019-01-24 18:33:43 +0100

    Dodano klase Bird

---
+++ listypython/Kurs Python/zoo/bird.py
@@ -0,0 +1,19 @@
+from Animal import Animal
+
+class Bird(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        self.kolor = "Czerwony"
+
+    def print_atrybuty(self):
+        print self.kolor
+        print self.gatunek
+        print self.imie
+        print self.waga
+        print self.wiek
+
+bird = Bird()
+
+bird.print_atrybuty()
+
+
